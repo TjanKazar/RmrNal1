2380131e35c1c5800ff836953a6ed67ae2434e04
TjanKazar <tjan.kazar3@gmail.com>
2024-10-16 20:52:34 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace RmrNal1
+{
+	public partial class MainPage : ContentPage
+	{
+		string cUser = "username";
+		string cPass = "geslo";
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		
+		private async void handleLogin(object sender, EventArgs e)
+		{
+			if (cUser == usernameField.Text && cPass == passwordField.Text)
+			{
+				await Navigation.PushAsync(new NewPage1());
+			}
+		}
+		private async void VizitkaOnClick(object sender, EventArgs e)
+		{
+			
+				await Navigation.PushAsync(new NewPage2());
+			
+		}
+
+	}
+}
---
+++ NewPage2.xaml.cs
@@ -0,0 +1,9 @@
+namespace RmrNal1;
+
+public partial class NewPage2 : ContentPage
+{
+	public NewPage2()
+	{
+		InitializeComponent();
+	}
+}
